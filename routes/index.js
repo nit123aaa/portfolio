dcf21e1505a8afcacb2324839266f0ae81f91c3a
unknown <nitishsingh12345677@gmail.com>
2025-02-01 21:43:11 +0530

    improve url

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/homeController");
 
-router.get("/app", homeController.index);
+router.get("/", homeController.index);
 router.get("/skills", homeController.skills);
 
 
